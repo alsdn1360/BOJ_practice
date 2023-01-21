87627ce854b518f6925e1348598392999e46acd6
Dongwoo Kang <110466553+alsdn1360@users.noreply.github.com>
2023-01-21 13:38:48 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,14 +5,20 @@ using namespace std;
 
 int main() {
     string hash;
-    int i, L, r = 31, M = 1234567891, result = 0;
+    int i, L;
+    long long r = 1, M = 1234567891, result = 0;
 
     cin >> L >> hash;
 
-
     for (i = 0; i < L; i++) {
-        result += (hash[i] - 96) * pow(r, i);
-        result = result % M;
+        if (r > M)
+            r = r % M;
+        if (result > M)
+            result = result % M;
+
+        result += (hash[i] - 96) * r;
+
+        r = r * 31;
     }
 
     cout << result;
